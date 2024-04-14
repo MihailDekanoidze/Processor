f3ec6e54660355d5f9761aed19f895d078a02a94
Mihail <142479856+MihailDekanoidze@users.noreply.github.com>
2024-04-14 00:19:10 +0000

    Assembler bug fixed & Add Dissasembler

---
+++ Disassembler/Disassembling.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../include/CommonIncludes.h"
+#include "../include/DisassemblerV3.h"
+
+main_info* main_info_ctor(text_info* byte_code)
+{
+    main_info* disasm = (main_info*)calloc(1, sizeof(main_info));
+    if (!disasm) return disasm;
+
+    disasm->byte_code = byte_code;
+    
+    disasm->disasm_code = text_info_ctor();
+
+    ERROR_PUT(!disasm->disasm_code, disasm->errors, DISASM_ERROR_CALLOC, disasm);
+
+    disasm->disasm_code->buffer = calloc(byte_code->elemcount * INT_LEN, sizeof(char));
+    disasm->disasm_code->elemcount = byte_code->elemcount * INT_LEN;
+
+    ERROR_PUT(!disasm->byte_code->buffer, disasm->errors, DISASM_ERROR_CALLOC, disasm);
+
+    disasm->disam_log = fopen("Disasm_log.txt", "w");
+
+    ERROR_PUT(!disasm->disam_log, disasm->errors,  DISASM_ERROR_OPEN_FILE, disasm);
+
+    disasm->errors = DISASM_NO_ERRORS;
+    return disasm; 
+}
+
+void main_info_dtor(main_info* disasm)
+{
+    if (!disasm) 
+    {
+        printf("Null ptr was given by %s\n", __PRETTY_FUNCTION__);
+        return;
+    }
+
+    text_info_dtor(disasm->disasm_code);
+    fclose(disasm->disam_log);
+
+    free(disasm);
+}
+
+disassembler_error byte_code_disasm(main_info* disasm)
+{
+    LOG_FUNCTION_BEGIN;
+
+    size_t elem_count   = disasm->byte_code->elemcount;
+    FILE* disasm_log    = disasm->disam_log;
+    int* byte_code      = (int*)disasm->byte_code->buffer;
+    char* disasm_code   = (char*)disasm->disasm_code->buffer;
+
+    for (size_t ip = 0; ip < elem_count; ip++)
+    {
+        fprintf(disasm_log, "byte_code[%zu] = %d\n", ip, byte_code[ip]);
+
+        switch (byte_code[ip])
+        {
+            #define ASM_COMMAND(name, id, size, dis)                                         \
+            case id:                                                                    \
+            {                                                                           \
+                sprintf(disasm_code, "%s", dis);                                       \
+                disasm_code += size;                                                    \
+                                                                                        \
+                if (id & (1 << 7))                                                      \
+                {                                                                       \
+                    sprintf(disasm_code++, "%c", ' ');                                  \
+                    ip++;                                                               \
+                    Argument* arg =  sprint_arg(disasm_code, byte_code + ip, id);       \
+                    disassembler_error error = arg->error;                              \
+                    fprintf(disasm_log, "arg is %d\n", *(byte_code + ip));              \
+                    disasm_code += arg->len;                                            \
+                    free(arg);                                                          \
+                    ERROR_PUT(error, disasm->errors, error, error);                     \
+                }                                                                       \
+                sprintf(disasm_code++, "\n");                                           \
+                break;                                                                  \
+                }                                                                                 
+            #include "../include/Command_Description.txt"                  
+            #undef ASM_COMMAND
+            case EMPTY_COMMAND: break;
+            default:
+            {   
+                printf("Unknown command = %d\n", byte_code[ip]);
+                ERROR_PUT(1, disasm->errors, DISASM_ERROR_UNKNOWN_COMMAND, DISASM_ERROR_UNKNOWN_COMMAND);
+            }
+        }
+    }
+
+    LOG_FUNCTION_END;
+    return DISASM_NO_ERRORS;
+}
+
+Argument* sprint_arg(char* disasm_code, int* byte_code, int command_id)
+{
+    Argument* arg   = (Argument*)calloc(1, sizeof(Argument));
+    arg->error      = DISASM_NO_ERRORS;
+
+    if (command_id & Num)
+    {
+        sprintf(disasm_code, "%d", *byte_code); 
+        arg->len = strlen(disasm_code);
+    }
+    else if (command_id & Reg)
+    {
+        arg->len = 2;
+        sprintf(disasm_code, "%cX", 'A' + *byte_code - 1);
+    }
+    else if (command_id & Mem)
+    {
+        sprintf(disasm_code, "[%d]", *byte_code); 
+        arg->len = strlen(disasm_code);
+    }
+    else
+    {
+        arg->error = DISASM_ERROR_UNKNOWN_ARG;
+    }
+
+    return arg;
+}
--- Disassembler/main.cpp
+++ Disassembler/main.cpp
@@ -1,189 +1,30 @@
 #include <stdio.h>
 #include <malloc.h>
+
+#include "../include/CommonIncludes.h"
 #include "../include/DisassemblerV3.h"
-#include "../include/Disasmreg.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    FILE* code = fopen("../Output/AssemblerArrCode", "rb");
-        // use onegin, put both files to the buffer first!
+    Errors error = NO_ERROR;
+    if (argc != 3) error = NO_DEFINED_FILE;
 
-    if (code == NULL)
-    {
-        return DISASM_ERROR_OPEN_FILE;
-    }
+    ERROR_CHECK(error, "Files was not defined ", );
 
-    FILE* disasm = fopen("../Output/DISASMCODE.txt", "w");
+    text_info* byte_code = text_info_ctor();
+    main_info* disasm    = NULL;
 
-    if (disasm  == NULL)
-    {
-        return DISASM_ERROR_OPEN_FILE;
-    }
+    error = file_to_buffer(argv[1], byte_code, sizeof(int));
+    ERROR_CHECK(error, "Input abort: error code ", DISASM_PROGRAMM_FINISH);
 
-    size_t size_code = fsize(code) / sizeof(int);
+    disasm = main_info_ctor(byte_code);
+    if (!disasm) error = NULL_PTR;
+    ERROR_CHECK(error, "Main_info disasm create failed: error code ", DISASM_PROGRAMM_FINISH);
+    disasm->errors = byte_code_disasm(disasm);
+    ERROR_CHECK(disasm->errors, "Main_info disasm create failed: error code ", DISASM_PROGRAMM_FINISH);
 
-    //printf("fsize: size_code = %zu\n", size_code);
-
-    int* asm_code = (int*) calloc(size_code, sizeof(int));
-
-    int fread_count = fread(asm_code, sizeof(int), size_code, code);
-
-    if (!fread_count)
-    {
-        return DISASM_ERROE_BYTE_CODE;
-    }
-
-    size_t ip = 0;
-
-    int command = 0;
-
-    while (command != HLT) // do while()
-    {
-    //printf("CMD: %d\n", command);
-    //printf("in while\n");
-
-    int command = 0;
-
-    for (size_t ip = 0; ip < size_code; ip++)
-    {
-        command = asm_code[ip];
-
-        command = asm_code[ip];
-
-        switch (command)
-        {
-            case COMMENT:
-            {
-                break;
-            }
-            case JMP_COMMAND:
-            {
-                fprintf(disasm, "%s ", jmp); 
-
-                int tempt = asm_code[++ip];
-                
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JA_COMMAND:
-            {
-                fprintf(disasm, "%s ", ja); 
-
-                int tempt = asm_code[++ip];
-
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JB_COMMAND:
-            {
-                fprintf(disasm, "%s ", jb); 
-
-                int tempt = asm_code[++ip];
-
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JE_COMMAND:
-            {
-                fprintf(disasm, "%s ", je); 
-
-                int tempt = asm_code[++ip];
-
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JNA_COMMAND:
-            {
-                fprintf(disasm, "%s ", jna);
-
-                int tempt = asm_code[++ip];
-                
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JNB_COMMAND:
-            {
-                fprintf(disasm, "%s ", jnb); 
-
-                int tempt = asm_code[++ip];
-                
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case JNE_COMMAND:   
-            {
-                fprintf(disasm, "%s ", jne); 
-
-                int tempt = asm_code[++ip];
-                
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }         
-            case PUSH:
-            {
-                fprintf(disasm, "%s ", push); 
-
-                int tempt = asm_code[++ip];
-                
-                fprintf(disasm, "%d\n", tempt);
-
-                break;
-            }
-            case ADD: fprintf(disasm, "ADD\n"); break;
-            case SUB: fprintf(disasm, "SUB\n"); break;
-            case DIV: fprintf(disasm, "DIV\n"); break;
-            case MUL: fprintf(disasm, "MUL\n"); break;
-            case OUT: fprintf(disasm, "OUT\n"); break;
-			case IN:  fprintf(disasm, "IN\n");  break;
-            case HLT:  fprintf(disasm, "HLT\n");  break;
-            case RPUSH:
-            {
-                    fprintf(disasm, "%s ", push);
-
-                    int tempt = asm_code[++ip];
-                
-                    // функцию
-                    if (DisasmPutRegtoFile(disasm, tempt) != 0)
-                    {
-                        return DISASM_ERROR_RPUSH_REG;
-                    }
-
-                    break;
-            }
-			case POP:
-            {
-                    fprintf(disasm, "%s ", pop);
-
-                    int tempt = asm_code[++ip];
-                
-                    if (DisasmPutRegtoFile(disasm, tempt) != 0)
-                    {
-                        return DISASM_ERROR_RPUSH_REG;
-                    }
-
-                    break;
-            }
-            default:
-            {
-                    printf("Command is %d\n", command);
-                    printf("\n<<<UNEXPECTED EXIT>>>\n");
-
-                    return DISASM_ERROR_UNKNOWN_COMMAND;
-
-                    break;
-            }
-        }
-        ip++;
-    }
-
-    fclose(disasm);
-
-    fclose(code);
+    error = byte_code_to_file(argv[2], disasm->disasm_code, sizeof(char));
+    ERROR_CHECK(error, "File writing failed: error code ", DISASM_PROGRAMM_FINISH);
+    
+    DISASM_PROGRAMM_FINISH;
 }
